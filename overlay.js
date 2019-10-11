9b696d5e96a97942614072b70b62cc195f0277c2
Bulut-D <42961652+Bulut-D@users.noreply.github.com>
2019-10-11 14:20:36 +0100

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -3,6 +3,7 @@
    videojs.registerPlugin('videoOverlay', function() {
        var player = this;
        player.el().appendChild('<p>hello</p>');
+       console.log("hit");
        console.log(player);
    });
 }(window.videojs));
