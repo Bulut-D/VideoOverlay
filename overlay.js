af38186c77e963fc3c097ede806a4de674a07923
Bulut-D <42961652+Bulut-D@users.noreply.github.com>
2019-10-11 14:24:41 +0100

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -1,9 +1,10 @@
 (function(videojs) {
   'use strict';
    videojs.registerPlugin('videoOverlay', function() {
-       var player = this;
-       player.el().appendChild('<p>hello</p>');
-       console.log("hit");
-       console.log(player);
+      var player = this,
+      overlay = document.createElement('p');
+      overlay.className = 'vjs-overlay';
+      overlay.innerHTML = "Becoming a plugin developer";
+      player.el().appendChild(overlay);
    });
 }(window.videojs));
