35b443e9b5688b111e839673da6d99cb65b3d8cd
Bulut-D <42961652+Bulut-D@users.noreply.github.com>
2019-10-11 14:32:20 +0100

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -2,9 +2,17 @@
   'use strict';
    videojs.registerPlugin('videoOverlay', function() {
       var player = this,
-      overlay = document.createElement('p');
-      overlay.className = 'vjs-overlay';
-      overlay.innerHTML = "Becoming a plugin developer";
+      overlay = document.createElement('div');
+      overlay.id = 'video_overlay';
+      overlay.innerHTML = "<div id='videoOverlayInner'></div>";
       player.el().appendChild(overlay);
+     
+     function checkForOverlay() {
+          var dataObj = player.mediainfo.customFields;
+          if (dataObj && (dataObj.video_overlay_product_icon || dataObj.video_overlay_title || dataObj.video_overlay_subtext)) {
+            console.log("trueeee");  
+            //videoOverlay.classList.add("overlay-fade-in");
+          }
+      }
    });
 }(window.videojs));
